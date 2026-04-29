0f581b641b7541d4d251193ac8ae1c0c2439d394
Webpageguy7 <comixkinggo@gmail.com>
2026-04-28 22:25:26 -0500

    Main Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 // main.js
 
 console.log("Hello, World!");
+
+
+// 1. Select the specific image by its ID
+const img = document.getElementById('myTargetImage');
+
+// 2. Add a click event listener
+img.addEventListener('click', function() {
+    // 3. Define what happens when clicked
+    alert('Head to the Next Page!');
+    console.log('You clicked the specific image.');
+});
+
