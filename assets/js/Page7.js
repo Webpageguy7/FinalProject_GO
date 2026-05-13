0a3405260d2020bb2700add5e17609e997d7905b
Webpageguy7 <comixkinggo@gmail.com>
2026-05-13 16:56:45 -0500

    Update Page7.js

--- assets/js/Page7.js
+++ assets/js/Page7.js
@@ -1,8 +1,9 @@
 let rotation = 0;
-setInterval(function () {
-  rotation += 3;
+document.addEventListener("click", function () {
+  let min = 1;
+  let max = 20;
+  rotation += Math.random() * (max - min + 1) + min;
   document.body.style.transform = `rotate(${rotation}deg)`;
-}, 100);
-  const img = document.getElementById('Puppy', 'Milk', 'Poster', 'Underpants');
+});
 
 document.getElementById("myText").textContent = "This is Safe Text.";
