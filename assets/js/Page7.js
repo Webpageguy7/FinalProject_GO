fd783cf969be48fc6f9c56d45aea7f1645083e02
Webpageguy7 <comixkinggo@gmail.com>
2026-05-13 10:26:01 -0500

    Added New JS
    Add the codenames for each object if needed

--- assets/js/Page7.js
+++ assets/js/Page7.js
@@ -1,7 +1,5 @@
 let rotation = 0;
-document.addEventListener("click", function () {
-  let min = 1;
-  let max = 50;
-  rotation += Math.random() * (max - min + 5) + min;
+setInterval(function () {
+  rotation += 5;
   document.body.style.transform = `rotate(${rotation}deg)`;
-});
+}, 100);
