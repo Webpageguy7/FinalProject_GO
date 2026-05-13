506731f6112ef76fe2f6a36605a192d086e40f55
Webpageguy7 <comixkinggo@gmail.com>
2026-05-13 16:07:08 -0500

    Added a prevent to text change

--- assets/js/Page7.js
+++ assets/js/Page7.js
@@ -1,6 +1,10 @@
-const img = document.getElementById('Puppy', 'Milk', 'Poster', 'Underpants');
 let rotation = 0;
 setInterval(function () {
   rotation += 5;
   document.body.style.transform = `rotate(${rotation}deg)`;
 }, 100);
+  const img = document.getElementById('Puppy', 'Milk', 'Poster', 'Underpants');
+
+ window.changeText = function() {
+        console.log("Text change blocked.");
+    };
